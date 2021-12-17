1bb5e7b68b73a2da28958e02ecfef4b94f90f294
Camilo Contreras <camilocontrerasvargas910@gmail.com>
2021-12-17 12:04:45 -0500

    cambios al main para lectura de images en cambio de videos

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from utils import *
 import pandas as pd
 import pickle
-cal = pickle.load(open("intrinsic.pickle", "rb"))
+import os
+cal = pickle.load(open("basler.pickle", "rb"))
 
 
-def main(root: str):
+def main(root):
     x_tip = []
     y_tip = []
     z_tip = []
@@ -12,34 +13,24 @@ def main(root: str):
     detector = Aruco()
     board = Pose(cal["mtx"], cal["dist"], 3, 19, [0, 4, 8, 16])  # in mm
 
-    frame_id = 0
-    frame_rate = 1
-    cap = cv2.VideoCapture(root)
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        frame_id += 1
-        if frame_id % frame_rate != 0:
-            continue
+    images = np.array([root + f for f in os.listdir(root) if f.endswith(".PNG")])
+    order = np.argsort([int(p.split(".")[-2].split("_")[-1]) for p in images])
+    images = images[order]
 
+    for im in images:
+        frame = cv2.imread(im)
         corners, ids = detector.detection(frame)
         detector.draw_detections(frame, corners, ids)
 
         r_, r_cube, t_cube = board.pose_board(corners, ids, 2)
-        r1_, r_cube1, t_cube1 = board.pose_board(corners, ids, 0)
+
         if r_ > 3:
-            p_tip = Tip(r_cube, t_cube)
+            p_tip = tip(r_cube, t_cube)
             board.draw_axis(frame, r_cube, p_tip, 5)
             board.draw_axis(frame, r_cube, t_cube, 15)
             x_tip.append(p_tip[0][0])
             y_tip.append(p_tip[1][0])
             z_tip.append(p_tip[2][0])
-
-        if r1_ > 3:
-            p_tip = Tip(r_cube1, t_cube1)
-            board.draw_axis(frame, r_cube1, p_tip, 5)
-            board.draw_axis(frame, r_cube1, t_cube1, 15)
 
         cv2.imshow('video', frame)
         if cv2.waitKey(1) & 0xFF == ord('s'):
@@ -49,6 +40,5 @@ def main(root: str):
     return pd.DataFrame(data={"x axis tip": x_tip, "y axis tip": y_tip, "z axis tip": z_tip})
 
 
-video = "/Users/pc/PycharmProjects/tg/main.mov"
-df = main(video)
-# df.to_excel(excel_writer="animation_blender.xlsx")
+folder = './refine_tip/'
+df = main(folder)
